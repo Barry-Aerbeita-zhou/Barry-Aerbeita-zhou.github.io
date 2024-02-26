d7dfbaf4a94894edf5c6bead6a61f9e813c696be
Aerbeita <2981571473@qq.com>
2024-02-26 16:51:50 +0800

    Site updated: 2024-02-26 16:51:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/23/hello-world/","2024/02/23/建站第一天/"];function toRandomPost(){
+var posts=["2024/02/26/Study/","2024/02/23/建站第一天/","2024/02/23/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
