304905ec6aaed20c099d1f670a0d08fb706aabb2
Aerbeita <2981571473@qq.com>
2024-02-29 16:52:09 +0800

    Site updated: 2024-02-29 16:52:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/23/hello-world/","2024/01/23/建站第一天/","2024/02/26/Study/"];function toRandomPost(){
+var posts=["2024/02/23/hello-world/","2024/02/26/Study/","2024/01/23/建站第一天/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
