aa19a920a11994c7823d83c511072fea053e1f22
Aerbeita <2981571473@qq.com>
2024-03-06 17:41:38 +0800

    Site updated: 2024-03-06 17:41:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/26/Study/","2024/01/23/建站第一天/","2024/02/23/hello-world/","2024/03/01/Markdown-基础学习/"];function toRandomPost(){
+var posts=["2024/03/06/Docker-入门学习/","2024/03/06/自建Nas的小笔记/","2024/01/23/建站第一天/","2024/03/01/Markdown-基础学习/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
