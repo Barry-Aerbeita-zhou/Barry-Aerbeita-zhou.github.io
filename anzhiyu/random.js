35c2658ecc87c7f9342a989cc5d8079be09ed998
Aerbeita <2981571473@qq.com>
2024-03-13 11:10:08 +0800

    Site updated: 2024-03-13 11:10:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/23/建站第一天/","2024/03/06/自建Nas的小笔记/","2024/03/06/Docker-入门学习/","2024/03/01/Markdown-基础学习/","2024/03/12/Debian能干嘛？/"];function toRandomPost(){
+var posts=["2024/03/12/Debian能干嘛？/","2024/01/23/建站第一天/","2024/03/06/自建Nas的小笔记/","2024/03/06/Docker-入门学习/","2024/03/01/Markdown-基础学习/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
