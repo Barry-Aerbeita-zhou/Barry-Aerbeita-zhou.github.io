171a1f08b1c89c50aaea7afb8526e13cf567c0b9
Aerbeita <2981571473@qq.com>
2024-05-09 16:51:23 +0800

    Site updated: 2024-05-09 16:50:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/06/Docker-入门学习/","2024/03/12/Debian能干嘛？/","2024/04/01/Hexo的常用指令/","2024/04/01/PHP自学手册/","2024/04/02/使用Hexo写作的正确姿势/","2024/01/23/建站第一天/","2024/03/06/自建Nas的小笔记/","2024/03/01/Markdown-基础学习/","2024/05/07/Java的Process-避坑小记/","2024/05/07/WSL（在-Windows-上安装-Linux）/"];function toRandomPost(){
+var posts=["2024/03/06/Docker-入门学习/","2024/03/12/Debian能干嘛？/","2024/04/01/Hexo的常用指令/","2024/04/01/PHP自学手册/","2024/04/02/使用Hexo写作的正确姿势/","2024/01/23/建站第一天/","2024/03/06/自建Nas的小笔记/","2024/03/01/Markdown-基础学习/","2024/05/07/WSL（在-Windows-上安装-Linux）/","2024/05/07/Java 的 Process-避坑小记/","2024/05/09/Front-matter-的基本认识/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
